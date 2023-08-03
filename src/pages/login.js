b85646f545662240c775f1a5f5d6bf99d9c05eb0
Renata Hafemann <mgs.renata@gmail.com>
2023-08-02 22:24:10 -0400

    added new account form with verification

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Signup from "../components/Login/signup";
+import NewAccount from "../components/Login/NewAccount";
 
 function Login(){
     return(
         <>
-            <Signup/>
+            <NewAccount/>
         </>
     )
 }
